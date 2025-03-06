ec78daa6dd5b5c593e35badba3a94ef0123de7e1
shishkarev <and01072004@gmail.com>
2025-03-07 00:16:52 +0300

    fix clang-tidy in mpi/perf_tests

--- tasks/mpi/dudchenko_o_shtrassen_algorithm/perf_tests/main.cpp
+++ tasks/mpi/dudchenko_o_shtrassen_algorithm/perf_tests/main.cpp
@@ -1,95 +1,108 @@
 #include <gtest/gtest.h>
 
+#include <boost/mpi/communicator.hpp>
 #include <boost/mpi/timer.hpp>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
 #include <random>
+#include <vector>
 
 #include "core/perf/include/perf.hpp"
+#include "core/task/include/task.hpp"
 #include "mpi/dudchenko_o_shtrassen_algorithm/include/ops_mpi.hpp"
 
-static std::vector<double> generate_random_square_matrix(int n, double minValue = -50.0, double maxValue = 50.0) {
+namespace{
+struct Value {
+  double min_value = -50;
+  double max_value = 50;
+}
+
+std::vector<double> generate_random_square_matrix(int n, Value value) {
   std::vector<double> matrix(n * n);
 
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_real_distribution<double> dis(minValue, maxValue);
+  std::uniform_real_distribution<double> dis(value.min_value, value.max_value);
 
   for (int i = 0; i < n * n; ++i) {
     matrix[i] = dis(gen);
   }
   return matrix;
 }
+}  //namepace
 
 TEST(dudchenko_o_shtrassen_algorithm_mpi, test_pipeline_run) {
   boost::mpi::communicator world;
-  const size_t N = 256;
+  const size_t n = 256;
 
-  std::vector<double> A = generate_random_square_matrix(N);
-  std::vector<double> B = generate_random_square_matrix(N);
-  std::vector<double> out(N * N, 0.0);
+  std::vector<double> a = generate_random_square_matrix(n);
+  std::vector<double> b = generate_random_square_matrix(n);
+  std::vector<double> out(n * n, 0.0);
 
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
   if (world.rank() == 0) {
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
-    taskDataPar->inputs_count.emplace_back(A.size());
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
-    taskDataPar->inputs_count.emplace_back(B.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-    taskDataPar->outputs_count.emplace_back(out.size());
+    task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t *>(a.data()));
+    task_data_par->inputs_count.emplace_back(a.size());
+    task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t *>(b.data()));
+    task_data_par->inputs_count.emplace_back(b.size());
+    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+    task_data_par->outputs_count.emplace_back(out.size());
   }
 
-  auto testTaskParallel = std::make_shared<dudchenko_o_shtrassen_algorithm_mpi::StrassenAlgoriphmParallel>(taskDataPar);
-  ASSERT_TRUE(testTaskParallel->ValidationImpl());
-  ASSERT_TRUE(testTaskParallel->PreProcessingImpl());
-  ASSERT_TRUE(testTaskParallel->RunImpl());
-  ASSERT_TRUE(testTaskParallel->PostProcessingImpl());
+  auto test_task_parallel = std::make_shared<dudchenko_o_shtrassen_algorithm_mpi::StrassenAlgoriphmParallel>(task_data_par);
+  ASSERT_TRUE(test_task_parallel->ValidationImpl());
+  ASSERT_TRUE(test_task_parallel->PreProcessingImpl());
+  ASSERT_TRUE(test_task_parallel->RunImpl());
+  ASSERT_TRUE(test_task_parallel->PostProcessingImpl());
 
-  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 2;
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 2;
   const boost::mpi::timer current_timer;
-  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  perf_attr->current_timer = [&] { return current_timer.elapsed(); };
 
-  auto perfResults = std::make_shared<ppc::core::PerfResults>();
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskParallel);
-  perfAnalyzer->PipelineRun(perfAttr, perfResults);
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_parallel);
+  perf_analyzer->PipelineRun(perf_attr, perf_results);
   if (world.rank() == 0) {
-    ppc::core::Perf::PrintPerfStatistic(perfResults);
+    ppc::core::Perf::PrintPerfStatistic(perf_results);
   }
 }
 
 TEST(dudchenko_o_shtrassen_algorithm_mpi, test_task_run) {
   boost::mpi::communicator world;
-  const size_t N = 256;
+  const size_t n = 256;
 
-  std::vector<double> A = generate_random_square_matrix(N);
-  std::vector<double> B = generate_random_square_matrix(N);
-  std::vector<double> out(N * N, 0.0);
+  std::vector<double> a = generate_random_square_matrix(n);
+  std::vector<double> b = generate_random_square_matrix(n);
+  std::vector<double> out(n * n, 0.0);
 
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
   if (world.rank() == 0) {
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
-    taskDataPar->inputs_count.emplace_back(A.size());
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
-    taskDataPar->inputs_count.emplace_back(B.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-    taskDataPar->outputs_count.emplace_back(out.size());
+    task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t *>(a.data()));
+    task_data_par->inputs_count.emplace_back(a.size());
+    task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t *>(b.data()));
+    task_data_par->inputs_count.emplace_back(b.size());
+    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+    task_data_par->outputs_count.emplace_back(out.size());
   }
 
-  auto testTaskParallel = std::make_shared<dudchenko_o_shtrassen_algorithm_mpi::StrassenAlgoriphmParallel>(taskDataPar);
-  ASSERT_TRUE(testTaskParallel->ValidationImpl());
-  ASSERT_TRUE(testTaskParallel->PreProcessingImpl());
-  ASSERT_TRUE(testTaskParallel->RunImpl());
-  ASSERT_TRUE(testTaskParallel->PostProcessingImpl());
+  auto test_task_parallel = std::make_shared<dudchenko_o_shtrassen_algorithm_mpi::StrassenAlgoriphmParallel>(task_data_par);
+  ASSERT_TRUE(test_task_parallel->ValidationImpl());
+  ASSERT_TRUE(test_task_parallel->PreProcessingImpl());
+  ASSERT_TRUE(test_task_parallel->RunImpl());
+  ASSERT_TRUE(test_task_parallel->PostProcessingImpl());
 
-  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 2;
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 2;
   const boost::mpi::timer current_timer;
-  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  perf_attr->current_timer = [&] { return current_timer.elapsed(); };
 
-  auto perfResults = std::make_shared<ppc::core::PerfResults>();
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskParallel);
-  perfAnalyzer->TaskRun(perfAttr, perfResults);
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_parallel);
+  perf_analyzer->TaskRun(perf_attr, perf_results);
 
   if (world.rank() == 0) {
-    ppc::core::Perf::PrintPerfStatistic(perfResults);
+    ppc::core::Perf::PrintPerfStatistic(perf_results);
   }
 }
