d78db6340dad55d445dbd9cb58726942184a7f37
shishkarev <and01072004@gmail.com>
2025-03-07 04:57:34 +0300

    6th debug mpi/func_tests 'CreateTest'

--- tasks/mpi/dudchenko_o_shtrassen_algorithm/func_tests/main.cpp
+++ tasks/mpi/dudchenko_o_shtrassen_algorithm/func_tests/main.cpp
@@ -121,21 +121,21 @@ void CreateTest(size_t n) {
   RunParallelTest(task_data_par);
 
   if (world.rank() == 0) {
-    std::cout << "Sequential output: ";
-    for (const auto &val : out_seq) {
-      std::cout << val << " ";
-    }
-    std::cout << std::endl;
+  std::cout << "Sequential output: ";
+  for (const auto &val : out_seq) {
+    std::cout << val << " ";
+  }
+  std::cout << std::endl;
 
-    std::cout << "Parallel output: ";
-    for (const auto &val : out_par) {
-      std::cout << val << " ";
-    }
-    std::cout << std::endl;
+  std::cout << "Parallel output: ";
+  for (const auto &val : out_par) {
+    std::cout << val << " ";
+  }
+  std::cout << std::endl;
   }
 
   for (size_t i = 0; i < n * n; i++) {
-    EXPECT_NEAR(out_seq[i], out_par[i], 1e-8);
+    ASSERT_NEAR(out_seq[i], out_par[i], 1e-8);
   }
 }
 }  // namespace
