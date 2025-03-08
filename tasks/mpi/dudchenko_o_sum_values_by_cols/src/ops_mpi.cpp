0c657bf747c61cda7abf44b6b862558ad4d0d482
shishkarev <and01072004@gmail.com>
2025-03-09 00:55:28 +0300

    fix all tests and src 2

--- tasks/mpi/dudchenko_o_sum_values_by_cols/src/ops_mpi.cpp
+++ tasks/mpi/dudchenko_o_sum_values_by_cols/src/ops_mpi.cpp
@@ -11,7 +11,7 @@
 bool dudchenko_o_sum_values_by_cols_mpi::SumValByColsMpi::PreProcessingImpl() {
   if (world_.rank() == 0) {
     input_ = std::vector<int>(task_data->inputs_count[0]);
-    auto *tmp = reinterpret_cast<int *>(task_data->inputs[0]);
+    auto* tmp = reinterpret_cast<int *>(task_data->inputs[0]);
     std::copy(tmp, tmp + task_data->inputs_count[0], input_.begin());
     rows_ = task_data->inputs_count[1];
     cols_ = task_data->inputs_count[2];
@@ -129,7 +129,7 @@ bool dudchenko_o_sum_values_by_cols_mpi::SumValByColsMpi::RunImpl() {
 bool dudchenko_o_sum_values_by_cols_mpi::SumValByColsMpi::PostProcessingImpl() {
   if (world_.rank() == 0) {
     for (unsigned int j = 0; j < cols_; j++) {
-      reinterpret_cast<int *>(task_data->outputs[0])[j] = sum_[j];
+      reinterpret_cast<int*>(task_data->outputs[0])[j] = sum_[j];
     }
   }
   return true;
