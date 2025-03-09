8fc0a377471891bc126230c5d4310a349c08aff0
shishkarev <and01072004@gmail.com>
2025-03-09 04:49:21 +0300

    fix to master 4

--- tasks/mpi/dudchenko_o_shtrassen_algorithm/include/ops_mpi.hpp
+++ tasks/mpi/dudchenko_o_shtrassen_algorithm/include/ops_mpi.hpp
@@ -2,7 +2,6 @@
 
 #include <boost/mpi/collectives.hpp>
 #include <boost/mpi/communicator.hpp>
-#include <boost/serialization/vector.hpp>  // NOLINT
 #include <cmath>
 #include <cstddef>
 #include <memory>
