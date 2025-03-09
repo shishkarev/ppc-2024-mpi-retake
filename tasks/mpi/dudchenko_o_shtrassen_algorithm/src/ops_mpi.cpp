821462a3d8f75a6822f23143ba9f711a96cfaf45
shishkarev <and01072004@gmail.com>
2025-03-09 05:09:43 +0300

    fix to master perf_tests 3

--- tasks/mpi/dudchenko_o_shtrassen_algorithm/src/ops_mpi.cpp
+++ tasks/mpi/dudchenko_o_shtrassen_algorithm/src/ops_mpi.cpp
@@ -5,6 +5,7 @@
 #include <boost/mpi/collectives.hpp>
 #include <boost/mpi/collectives/broadcast.hpp>
 #include <boost/mpi/collectives/reduce.hpp>
+#include <boost/serialization/vector.hpp>
 #include <cmath>
 #include <cstddef>
 #include <functional>
