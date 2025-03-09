610e44e4b9a9d721e0f8fd7e899189547181d2b9
shishkarev <and01072004@gmail.com>
2025-03-09 03:25:00 +0300

    fix all tests and src 6

--- tasks/mpi/dudchenko_o_sum_values_by_cols/src/ops_mpi.cpp
+++ tasks/mpi/dudchenko_o_sum_values_by_cols/src/ops_mpi.cpp
@@ -80,7 +80,9 @@ bool dudchenko_o_sum_values_by_cols_mpi::SumValByColsMpi::ValidationImpl() {
 //   local_input_.resize(rows_ * local_n);
 
 //   // Scatter the columns of the matrix to each process
-//   boost::mpi::scatterv(world_, input_.data(), send_counts, displs, local_input_.data(), send_counts[world_.rank()], 0);
+//   boost::mpi::scatterv(world_, input_.data(), send_counts, displs, local_input_.data(), send_counts[world_.rank()],
+//   0);
+
 
 //   // Calculate local sum for the assigned columns
 //   std::vector<int> local_sum(local_n, 0);
